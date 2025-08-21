e1fe47a8e98fda6adae4fad182ef1a02fe8a68b3
dem2k <dem@deminator.de>
2025-08-21 11:56:13 +0200

    Update ls.aliases.sh

--- aliases/ls.aliases.sh
+++ aliases/ls.aliases.sh
@@ -3,10 +3,11 @@
 
 # Directory Listing aliases
 alias dir='ls -hFx'
+alias ll='ls -laFh'             # Preferred 'ls' implementation
 alias l.='ls -d .* --color=tty' # short listing, only hidden files - .*
-alias l='ls -lathF'             # long, sort by newest to oldest
+alias l='ls -lFh'               # long, sort by newest to oldest
 alias L='ls -latrhF'            # long, sort by oldest to newest
-alias la='ls -Al'               # show hidden files
+alias la='ls -AlF'              # show hidden files
 alias lc='ls -lcr'              # sort by change time
 alias lk='ls -lSr'              # sort by size
 alias lh='ls -lSrh'             # sort by size human readable
@@ -16,10 +17,10 @@ alias lr='ls -lR'               # recursive ls
 alias lt='ls -ltr'              # sort by date
 alias lu='ls -lur'              # sort by access time
 
-
 #   lr:  Full Recursive Directory Listing
 #   ------------------------------------------
 alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'' | less'
 
-alias dud='du -d 1 -h'                      # Short and human-readable directory listing
-alias duf='du -sh *'                        # Short and human-readable file listing
+alias duh='du -h -d1 |sort -hr'    # Short and human-readable directory listing
+alias duf='du -sh *'               # Short and human-readable file listing
+alias dud='du -h -d1'
