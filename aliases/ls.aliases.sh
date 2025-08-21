43fc2854ae4fc732f50ca18cc8c4eac925aef25b
dem2k <dem@deminator.de>
2025-08-21 12:07:38 +0200

    Update ls.aliases.sh

--- aliases/ls.aliases.sh
+++ aliases/ls.aliases.sh
@@ -2,20 +2,20 @@
 #  ---------------------------------------------------------------------------
 
 # Directory Listing aliases
-alias dir='ls -hFx'
-alias ll='ls -laFh'             # Preferred 'ls' implementation
-alias l.='ls -d .* --color=tty' # short listing, only hidden files - .*
-alias l='ls -lFh'               # long, sort by newest to oldest
-alias L='ls -latrhF'            # long, sort by oldest to newest
-alias la='ls -AlF'              # show hidden files
-alias lc='ls -lcr'              # sort by change time
-alias lk='ls -lSr'              # sort by size
-alias lh='ls -lSrh'             # sort by size human readable
-alias lm='ls -al | more'        # pipe through 'more'
-alias lo='ls -laSFh'            # sort by size largest to smallest
-alias lr='ls -lR'               # recursive ls
-alias lt='ls -ltr'              # sort by date
-alias lu='ls -lur'              # sort by access time
+alias dir='ls -xFhv'
+alias ll='ls -laFhv'               # Preferred 'ls' implementation
+alias l.='ls -dFhv .* --color=tty' # short listing, only hidden files - .*
+alias l='ls -lFh'                  # long, sort by newest to oldest
+alias L='ls -latrFh'               # long, sort by oldest to newest
+alias la='ls -AlFhv'               # show hidden files
+alias lc='ls -lcrF'                # sort by change time
+alias lk='ls -lSrF'                # sort by size
+alias lh='ls -lSrFh'               # sort by size human readable
+alias lm='ls -alFhv | more'        # pipe through 'more'
+alias lo='ls -laSFhv'              # sort by size largest to smallest
+alias lr='ls -lRFhv'               # recursive ls
+alias lt='ls -ltrFhv'              # sort by date
+alias lu='ls -lurFhv'              # sort by access time
 
 #   lr:  Full Recursive Directory Listing
 #   ------------------------------------------
